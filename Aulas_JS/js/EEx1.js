d8b04e70a3a4485d0d30ac27ba5a56684b4d7326
Livia <146363895+Livia-mc@users.noreply.github.com>
2025-08-14 08:31:55 -0300

    Delete Aulas_JS directory

--- Aulas_JS/js/EEx1.js
+++
@@ -1,9 +0,0 @@
-var n1, n2, result1, result2
-
-n1= parseInt(prompt("Digite o primeiro valor "))
-n2= parseInt(prompt("Digite o segundo valor"))
-result1= n1 - n2
-result2= n2 - n1
-
-alert("O resuldado de "+n1+"-"+n2+" é = "+result1)
-alert("O resultado de "+n2+"-"+n1+" é = "+result2)
--- Aulas_JS/js/EEx3.js
+++
@@ -1,4 +0,0 @@
-var n1, result
-n1= parseInt(prompt("Digite o número: "))
-result = n1*n1*n1
-alert(n1+" ao cubo é = "+result)
