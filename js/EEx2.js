0f537d92cad3c4312a7e304cf0fb4cdcb19bab5a
Livia <146363895+Livia-mc@users.noreply.github.com>
2025-08-14 08:35:50 -0300

    Add files via upload

---
+++ js/EEx2.js
@@ -0,0 +1,8 @@
+var n1, n2, n3, result
+n1= parseInt(prompt("Digite o primeiro valor "))
+n2= parseInt(prompt("Digite o segundo valor "))
+n3= parseInt(prompt("Digite o terceiro valor "))
+
+result=n1*n2*n3
+
+alert("O resultado da multiplicação desse números é = "+result)
